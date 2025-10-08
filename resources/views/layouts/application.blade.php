6b81949565bb37274c09e8a8e28baf4f54a7a35a
abt2484 <alejandrobruno.buenaventura@inslapineda.cat>
2025-10-08 11:54:10 +0200

    Actualizacion header menu

--- resources/views/layouts/application.blade.php
+++ resources/views/layouts/application.blade.php
@@ -8,6 +8,7 @@
 </head>
 <body class="bg-[#FFF9F6]">
     <header class="flex items-start">
+        <!-- Menu lateral -->
         <nav class="bg-white h-screen p-5 shadow-sm flex flex-col items-center z-0 ">
             <ul class="flex flex-col gap-10">
                 <li class="mb-5 pt-1">
@@ -48,13 +49,46 @@
 
             </ul>
         </nav>
-        <div class="bg-white flex flex-row items-center gap-2 w-full p-5 shadow-sm pl-12">
+
+        <!-- Menu superior -->
+        <div class="bg-white flex flex-row items-center gap-2 w-full p-2 shadow-sm pl-12">
+
             <img src="{{ asset("images/vallparadis-logo.svg") }}" alt="vallparadis-logo" class="w-56 mr-10">
 
-            <form action="#" method="post" class="w-[60%]">
+            <form action="#" method="post" class="w-[65%] flex items-center gap-2 border-1 border-[#E6E5DE] rounded-lg h-10 bg-[#FFF9F6] p-5">
                 @csrf
-                <input type="search" name="search" id="search" placeholder="Buscar professionals , documents...." class="border-1 border-[#E6E5DE] rounded-lg h-10 bg-[#FFF9F6] w-full p-5 pl-14">
+
+                <button type="submit" class="cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#013148" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                </button>
+
+                <input type="search" name="search" id="search" placeholder="Buscar professionals , documents...." class=" pl-2 w-full h-10">
             </form>
+
+            <div class="w-[25%] flex flex-row items-center justify-between px-10">
+
+                <!-- Campana -->
+                <div class="relative">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                    </svg>
+
+                    <div class="h-6 w-6 rounded-full bg-orange-400 text-white absolute -top-3 -right-3 text-center font-bold flex items-center justify-center text-sm">
+                        <p>+11</p>
+                    </div>
+                </div>
+
+                <div class="flex flex-row items-center gap-5 ">
+                    <a href="#Perfil" class="w-14 h-14 rounded-full bg-green-200">
+                        <img src="#FotoPerfil" alt="#Fotoperfil">
+                    </a>
+    
+                    <p class="font-bold">Juan Lopez</p>
+                </div>
+
+            </div>
         </div>
     </header>
     <main>
