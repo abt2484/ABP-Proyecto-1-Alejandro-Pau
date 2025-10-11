3fe910cfc1196078b77656385f10821ae601b7d2
abt2484 <alejandrobruno.buenaventura@inslapineda.cat>
2025-10-11 10:55:58 +0200

    Comienzo del apartado de gestion de centros #12

--- resources/views/centers/index.blade.php
+++ resources/views/centers/index.blade.php
@@ -1,10 +1,80 @@
+@extends("layouts.application")
 
+@section("main")
 @if (session("success"))
     <p>{{ session("success") }}</p>
 @endif
 
 
-<h1>Centros: </h1>
+<h1 class="title">Gestió de centres: </h1>
+<div class="w-full flex flex-wrap flex-row justify-between items-stretch">
+    <!-- Contenedor -->
+    <div class="shadow-md simple-container w-96 mb-10">
+        <div class="flex justify-between items-center">
+            <p class="principal-text-color font-bold card-title">Centres</p>
+            <div class="bg-[#FF7E13] rounded-lg p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" class="size-8">mlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#012F4A" class="size-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />ath stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                </svg>
+            </div>
+        </div>
+        <p class="text-3xl text-left font-bold py-5">3</p>
+        
+        <p class="font-bold text-[#335C68] text-md text-left">Centres registrats al sistema</p>
+    </div>
+    
+    <!-- Contenedor -->
+    <div class="shadow-md simple-container w-96 mb-10">
+        <div class="flex justify-between items-center">
+            <p class="principal-text-color font-bold card-title">Centres nous</p>
+            <div class="bg-[#FF7E13] rounded-lg p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" class="size-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+            </div>
+
+        </div>
+        <p class="text-3xl text-left font-bold py-5">1</p>        
+        <p class="font-bold text-[#335C68] text-md text-left">Afegits durant l'últim mes</p>
+    </div>
+
+    <!-- Contenedor -->
+    <div class="shadow-md simple-container w-96 mb-10">
+        <div class="flex justify-between items-center">
+            <p class="principal-text-color font-bold card-title">Centres actius</p>
+            <div class="bg-green-600 rounded-lg p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" class="size-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+            </div>
+
+        </div>
+        <p class="text-3xl text-left font-bold py-5">2</p>
+        <p class="font-bold text-[#335C68] text-md text-left">Centres registrats al sistema</p>
+        <div>
+            <p>Barra de progreso</p>
+        </div>        
+    </div>
+
+    <!-- Contenedor -->
+    <div class="shadow-md simple-container w-96 mb-10">
+        <div class="flex justify-between items-center">
+            <p class="principal-text-color font-bold card-title">Centres inactius</p>
+            <div class="bg-red-600 p-2 rounded-lg">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" class="size-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+            </div>
+
+        </div>
+        <p class="text-3xl text-left font-bold py-5">1</p>        
+        <p class="font-bold text-[#335C68] text-md text-left">Centres registrats al sistema</p>
+        <p>Barra de progreso</p>
+    </div>
+
+
+
+</div>
 @foreach ($centers as $center )
     <a href="{{ route("centers.show", $center->id) }}">{{ $center->name }}</a>
     <br>
@@ -20,3 +90,5 @@
     
     <button type="submit" name="submit" id="submit">Eliminar</button>
 </form>
+
+@endsection
