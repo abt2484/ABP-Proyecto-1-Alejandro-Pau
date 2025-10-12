38c1c1f01083a16c30e78b2896d6bf054e68eb90
abt2484 <alejandrobruno.buenaventura@inslapineda.cat>
2025-10-12 21:12:38 +0200

    Actualizacion dashboard #18

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,13 +13,12 @@
         
         <div class="flex flex-col gap-2 sm:flex-row sm:justify-between sm:items-center">
             <p class="font-bold text-[#335C68] text-md text-left">4 nous aquest mes</p>
-            <button type="button" class="btn-primary w-full sm:w-32 text-sm shadow-md">
+            <a href="{{ route("users.index") }}" class="btn-primary w-full sm:w-32 text-sm shadow-md">
                 Accedir
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                <svg class="w-4 h-4">
+                    <use xlink:href="#icon-up-right-arrow"></use>
                 </svg>
-
-            </button>
+            </a>
         </div>
     </div>
 
@@ -27,9 +26,9 @@
     <div class="shadow-md simple-container w-96 mb-10">
         <div class="flex justify-between items-center">
             <p class="principal-text-color font-bold card-title">Centres</p>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#012F4A" class="size-8">mlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#012F4A" class="size-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />ath stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
-            </svg>
+                <svg class="w-8 h-8 text-[#012F4A]">
+                    <use xlink:href="#icon-center"></use>
+                </svg>
         </div>
         <p class="text-3xl text-left font-bold py-5">12</p>
         
@@ -49,9 +48,9 @@
     <!-- Contenedor -->
     <div class="shadow-md simple-container w-96 mb-10">
         <div class="flex justify-between items-center">
-            <p class="principal-text-color font-bold card-title">Seguiments oberts</p>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#012F4A" class="size-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+            <p class="principal-text-color font-bold card-title">Projectes/Comissions</p>
+            <svg class="w-8 h-8 text-[#012F4A]">
+                <use xlink:href="#icon-folder"></use>
             </svg>
         </div>
         <p class="text-3xl text-left font-bold py-5">6</p>
@@ -98,8 +97,8 @@
     <!-- Container -->
     <div class="simple-container shadow-lg flex flex-col items-start w-80">
         <div class="bg-blue-50 rounded-lg p-2 mb-3">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#155dfc" class="size-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+            <svg class="w-8 h-8 text-blue-600">
+                <use xlink:href="#icon-users"></use>
             </svg>
         </div>
 
@@ -109,38 +108,38 @@
         <!-- Opciones -->
         <ul class="my-5 w-full">
             <li>
-                <a href="google.com" class="list-option justify-between">
-                    Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <a href="{{ route("users.create") }}" class="list-option justify-between">
+                    Alta d'un professional nou
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
             <li>
-                <a href="google.com" class="list-option justify-between">
-                    Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <a href="{{ route("users.index") }}" class="list-option justify-between">
+                    Professional actius
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
             <li>
-                <a href="google.com" class="list-option justify-between">
-                    Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <a href="{{ route("users.index") }}" class="list-option justify-between">
+                    Professional inactius
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
         </ul>
 
-        <a href="#test" class="btn-primary w-full p-3 font-bold">Accedir al apartat</a>
+        <a href="{{ route("users.index") }}" class="btn-primary w-full p-3 font-bold">Accedir al apartat</a>
     </div>
     <!-- Container -->
     <div class="simple-container shadow-lg flex flex-col items-start w-80">
         <div class="bg-green-50 rounded-lg p-2 mb-3">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 stroke-green-600">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
+            <svg class="w-8 h-8 text-green-600">
+                <use xlink:href="#icon-center"></use>
             </svg>
         </div>
 
@@ -152,25 +151,25 @@
             <li>
                 <a href="{{ route("centers.create") }}" class="list-option justify-between">
                     Alta d'un centre nou
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
             <li>
                 <a href="#centresActius" class="list-option justify-between">
                     Centres actius
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
             <li>
                 <a href="#centresInActius" class="list-option justify-between">
                     Centres inactius
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
         </ul>
@@ -179,8 +178,8 @@
     <!-- Container -->
     <div class="simple-container shadow-lg flex flex-col items-start w-80">
         <div class="bg-teal-50 rounded-lg p-2 mb-3">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 stroke-teal-600">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+            <svg class="w-8 h-8 text-teal-600">
+                <use xlink:href="#icon-folder"></use>
             </svg>
 
         </div>
@@ -191,40 +190,38 @@
         <!-- Opciones -->
         <ul class="my-5 w-full">
             <li>
-                <a href="google.com" class="list-option justify-between">
-                    Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <a href="{{ route("projects.create") }}" class="list-option justify-between">
+                    Alta d'un nou projecte/comissió
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
             <li>
-                <a href="google.com" class="list-option justify-between">
-                    Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <a href="{{ route("projects.index") }}" class="list-option justify-between">
+                    Projectes
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
             <li>
-                <a href="google.com" class="list-option justify-between">
-                    Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <a href="{{ route("projects.index") }}" class="list-option justify-between">
+                    Comissions
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
         </ul>
-        <a href="#test" class="btn-primary w-full p-3 font-bold">Accedir al apartat</a>
+        <a href="{{ route("projects.index") }}" class="btn-primary w-full p-3 font-bold">Accedir al apartat</a>
 
     </div>
     <!-- Container -->
     <div class="simple-container shadow-lg flex flex-col items-start w-80">
         <div class="bg-[#fee9d6] rounded-lg p-2 mb-3">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-8 stroke-[#FF7E13]">
-                <path d="M3 2v7c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2V2"></path>
-                <path d="M7 2v20"></path>
-                <path d="M21 15V2a5 5 0 0 0-5 5v6c0 1.1.9 2 2 2h3Zm0 0v7"></path>
+            <svg class="w-8 h-8 text-[#FF7E13]">
+                <use xlink:href="#icon-knife"></use>
             </svg>
         </div>
 
@@ -236,25 +233,25 @@
             <li>
                 <a href="google.com" class="list-option justify-between">
                     Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
             <li>
                 <a href="google.com" class="list-option justify-between">
                     Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
             <li>
                 <a href="google.com" class="list-option justify-between">
                     Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
         </ul>
@@ -277,25 +274,25 @@
             <li>
                 <a href="google.com" class="list-option justify-between">
                     Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
             <li>
                 <a href="google.com" class="list-option justify-between">
                     Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
             <li>
                 <a href="google.com" class="list-option justify-between">
                     Nou professional
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
+                <svg class="w-6 h-6 text-[#012F4A]">
+                    <use xlink:href="#icon-no-line-arrow"></use>
+                </svg>
                 </a>
             </li>
         </ul>
