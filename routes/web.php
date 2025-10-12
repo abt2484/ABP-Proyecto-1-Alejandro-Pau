835d1b60d4895f231885e8f15344cbbba52f7e77
Pa-u-G <pau.garbajosa@inslapineda.cat>
2025-10-12 17:28:45 +0200

    conflicto arreglado #14

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@ Route::get('/', function () {
 });
 
 
-Route::resource("centers", CenterController::class);
 // Centros
 Route::resource("centers", CenterController::class)->except("destroy");
 Route::patch("/centers/{center}/disable", [CenterController::class, "disable"])->name("centers.disable");
