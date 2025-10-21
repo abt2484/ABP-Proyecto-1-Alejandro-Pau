a7ac7ac86acdac578b39d28afb25233fc1c2374f
abt2484 <alejandrobruno.buenaventura@inslapineda.cat>
2025-10-21 09:17:44 +0200

    Arreglo JS #47

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,4 @@
 import "./notifications"
 import "./toggleMenu"
 import "./excel"
-import "./projects"
 import { minidenticonSvg } from 'https://cdn.jsdelivr.net/npm/minidenticons@4.2.1/minidenticons.min.js'
