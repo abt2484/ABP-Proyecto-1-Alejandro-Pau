41dfabf4c27c32793747a9e21635f925eb4348a1
Pa-u-G <pau.garbajosa@inslapineda.cat>
2025-10-21 11:57:00 +0200

    foto perfil

--- resources/js/excel.js
+++ resources/js/excel.js
@@ -1,6 +1,9 @@
-document.getElementById('redirectSelect').addEventListener('change', function() {
-    const url = this.value;
-    if (url) {
-        window.location.href = url;
-    }
-});
+const redirectSelect= document.getElementById('redirectSelect')
+if (redirectSelect){
+    redirectSelect.addEventListener('change', function() {
+        const url = this.value;
+        if (url) {
+            window.location.href = url;
+        }
+    });
+}
