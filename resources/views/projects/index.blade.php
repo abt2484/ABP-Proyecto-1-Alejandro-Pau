56797a1ced304e66bf2f4433c0afda67bffb8fa8
Pa-u-G <pau.garbajosa@inslapineda.cat>
2025-10-27 12:51:55 +0100

    he movido de sitio el active/inactive de los proyectos #14

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -117,32 +117,32 @@
                 <div class="flex flex-row justify-between items-center w-full mb-2">
                     <div class="flex flex-row justify-between gap-2">
                         @if($project->type_label == "Projecte")
-                            <div class="bg-green-200 border-green-600 border-1 rounded-full p-4 flex items-center justify-center">
+                            <div class="bg-green-200 rounded-lg p-2">
                                 <svg class="w-8 h-8 text-green-600">
                                     <use xlink:href="#icon-folder"></use>
                                 </svg>
                             </div>
                         @elseif($project->type_label == "Comissió")
-                            <div class="bg-[#FF7033]/17 border-1 border-[#FF7033] rounded-full p-4 flex items-center justify-center">
+                            <div class="bg-[#FF7033]/17 rounded-lg p-2">
                                 <svg class="w-8 h-8 text-[#FF7033]">
                                     <use xlink:href="#icon-folder"></use>
                                 </svg>
                             </div>
                         @endif
-                        <div class="flex flex-col items-start gap-2">
+                        <div class="flex flex-row items-center gap-5">
                             <a href="{{ route('projects.show', $project->id) }}" class="text-[#012F4A] font-bold text-[20px]">{{ $project->name }}</a>
-                            <p class="p-1 text-white w-16 text-center rounded-lg {{ $project->is_active ? 'bg-green-600' : 'bg-red-600' }}"> {{ $project->is_active ? "Actiu" : "Inactiu" }}</p>
+                            @if($project->type_label == "Projecte")
+                                <div class="border-1 p-1 text-center bg-green-200 text-green-600 border-green-600 rounded-lg w-24 ">
+                                    Projecte
+                                </div>
+                            @elseif($project->type_label == "Comissió")
+                                <div class="border-1 bg-[#FF7033]/17 text-[#FF7033] border-[#FF7033] rounded-lg p-1 text-center w-24">
+                                    Comissió
+                                </div>
+                            @endif
                         </div>
                     </div>
-                    @if($project->type_label == "Projecte")
-                        <div class="border-1 p-1 text-center bg-green-200 text-green-600 border-green-600 rounded-lg w-24 ">
-                            Projecte
-                        </div>
-                    @elseif($project->type_label == "Comissió")
-                        <div class="border-1 bg-[#FF7033]/17 text-[#FF7033] border-[#FF7033] rounded-lg p-1 text-center w-24">
-                            Comissió
-                        </div>
-                    @endif
+                    <p class="p-1 text-white w-16 text-center rounded-lg {{ $project->is_active ? 'bg-green-600' : 'bg-red-600' }}"> {{ $project->is_active ? "Actiu" : "Inactiu" }}</p>
                 </div>
                 
                 <div class="mt-3 ml-5 space-y-5 text-[#011020]">
