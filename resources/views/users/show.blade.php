76ed2be75135c6bc6fcf5879542e1be12dbc3de7
Pa-u-G <pau.garbajosa@inslapineda.cat>
2025-11-10 13:24:16 +0100

    changes

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -199,7 +199,9 @@
                     <p class="text-2xl font-bold text-[#FF7033]">{{ $user->locker}}</p>
                 </div>
             </div>
-            <a href="{{ route("trackings.index", $user->id) }}" class="bg-[#FF7E13] text-white rounded-lg p-2 font-semibold flex items-center justify-center cursor-pointer gap-2 hover:bg-[#FE712B] transition-all h-fit">Seguiment</a>
+            <div>
+                <a href="{{ route("trackings.index", $user->id) }}" class="bg-[#FF7E13] text-white rounded-lg p-2 font-semibold flex items-center justify-center cursor-pointer gap-2 hover:bg-[#FE712B] transition-all h-fit">Seguiment</a>
+            </div>
             <div class="items-start h-full">
                 <a href="{{ route("evaluations.index", $user->id) }}" class="bg-green-600 text-white rounded-lg p-2 font-semibold flex items-center justify-center cursor-pointer gap-2 hover:bg-green-700 transition-all h-fit">Evaluacions</a>
             </div>
