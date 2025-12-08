b530a886950055b5779d52d0d5c844f71e8be450
abt2484 <alejandrobruno.buenaventura@inslapineda.cat>
2025-12-08 22:14:51 +0100

    Inicio contactos externos #102

---
+++ database/migrations/2025_12_08_205457_create_external_contacts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('external_contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string("center_id");
+            $table->enum("category", ["assistencial ", "serveis_generals"]);
+            $table->string("reason");
+            $table->string("company_or_department");
+            $table->string("contact_person");
+            $table->string("phone")->nullable();
+            $table->string("email");
+            $table->boolean("is_active")->default(true);
+            $table->text("observations")->nullable();
+            $table->timestamps();
+
+            $table->foreign("center_id")->references("id")->on("centers");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('external_contacts');
+    }
+};
