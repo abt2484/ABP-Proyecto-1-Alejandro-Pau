cb44d55c4796d5009f2f7cd253327cadba9b6197
abt2484 <alejandrobruno.buenaventura@inslapineda.cat>
2025-12-27 21:55:36 +0100

    Finalizacion seeders restantes #117

--- database/seeders/UniformitySeeder.php
+++ database/seeders/UniformitySeeder.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-
 class UniformitySeeder extends Seeder
 {
     /**
@@ -16,96 +15,245 @@ class UniformitySeeder extends Seeder
     {
         DB::table("uniformities")->insert([
             [
-                "id" => 1,
                 "user" => 1,
                 "shirt" => "L",
-                "pants" => "M",
-                "shoes" => 39.5,
-                "created_at" => now()->subMonths(12),
-                "updated_at" => now()->subMonths(12)
-            ],
-            [
-                "id" => 2,
-                "user" => 2,
-                "shirt" => "XL",
                 "pants" => "L",
                 "shoes" => 40.0,
-                "created_at" => now()->subMonths(11),
-                "updated_at" => now()->subMonths(11)
+                "created_at" => now(),
+                "updated_at" => now()
             ],
             [
-                "id" => 3,
-                "user" => 3,
-                "shirt" => "M",
-                "pants" => "M",
+                "user" => 2,
+                "shirt" => "L",
+                "pants" => "XXL",
                 "shoes" => 41.0,
-                "created_at" => now()->subMonths(10),
-                "updated_at" => now()->subMonths(10)
+                "created_at" => now(),
+                "updated_at" => now()
             ],
             [
-                "id" => 4,
+                "user" => 3,
+                "shirt" => "S",
+                "pants" => "M",
+                "shoes" => 38.5,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
                 "user" => 4,
+                "shirt" => "XL",
+                "pants" => "XXL",
+                "shoes" => 43.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 5,
+                "shirt" => "M",
+                "pants" => "L",
+                "shoes" => 39.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 6,
                 "shirt" => "L",
                 "pants" => "XL",
                 "shoes" => 42.5,
-                "created_at" => now()->subMonths(9),
-                "updated_at" => now()->subMonths(9)
+                "created_at" => now(),
+                "updated_at" => now()
             ],
             [
-                "id" => 5,
-                "user" => 5,
-                "shirt" => "XL",
-                "pants" => "L",
-                "shoes" => 40.5,
-                "created_at" => now()->subMonths(8),
-                "updated_at" => now()->subMonths(8)
-            ],
-            [
-                "id" => 6,
-                "user" => 6,
-                "shirt" => "3XL",
-                "pants" => "XXL",
-                "shoes" => 43.0,
-                "created_at" => now()->subMonths(7),
-                "updated_at" => now()->subMonths(7)
-            ],
-            [
-                "id" => 7,
                 "user" => 7,
+                "shirt" => "S",
+                "pants" => "S",
+                "shoes" => 37.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 8,
+                "shirt" => "XXL",
+                "pants" => "3XL",
+                "shoes" => 44.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 9,
                 "shirt" => "M",
                 "pants" => "M",
-                "shoes" => 38.5,
-                "created_at" => now()->subMonths(6),
-                "updated_at" => now()->subMonths(6)
+                "shoes" => 40.5,
+                "created_at" => now(),
+                "updated_at" => now()
             ],
             [
-                "id" => 8,
-                "user" => 8,
-                "shirt" => "L",
-                "pants" => "L",
-                "shoes" => 41.5,
-                "created_at" => now()->subMonths(5),
-                "updated_at" => now()->subMonths(5)
-            ],
-            [
-                "id" => 9,
-                "user" => 9,
-                "shirt" => "XL",
-                "pants" => "XL",
-                "shoes" => 42.0,
-                "created_at" => now()->subMonths(4),
-                "updated_at" => now()->subMonths(4)
-            ],
-            [
-                "id" => 10,
                 "user" => 10,
                 "shirt" => "L",
                 "pants" => "L",
+                "shoes" => 41.5,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 11,
+                "shirt" => "XL",
+                "pants" => "XL",
+                "shoes" => 42.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 12,
+                "shirt" => "S",
+                "pants" => "M",
+                "shoes" => 39.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 13,
+                "shirt" => "M",
+                "pants" => "L",
+                "shoes" => 40.5,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 14,
+                "shirt" => "L",
+                "pants" => "XL",
+                "shoes" => 41.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 15,
+                "shirt" => "XL",
+                "pants" => "XXL",
+                "shoes" => 43.5,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 16,
+                "shirt" => "S",
+                "pants" => "S",
+                "shoes" => 36.5,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 17,
+                "shirt" => "M",
+                "pants" => "L",
                 "shoes" => 40.0,
-                "created_at" => now()->subMonths(3),
-                "updated_at" => now()->subMonths(3)
-            ]       
-
-            ]);
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 18,
+                "shirt" => "L",
+                "pants" => "XL",
+                "shoes" => 42.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 19,
+                "shirt" => "XL",
+                "pants" => "XXL",
+                "shoes" => 43.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 20,
+                "shirt" => "S",
+                "pants" => "M",
+                "shoes" => 38.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 21,
+                "shirt" => "M",
+                "pants" => "L",
+                "shoes" => 40.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 22,
+                "shirt" => "L",
+                "pants" => "XL",
+                "shoes" => 41.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 23,
+                "shirt" => "XL",
+                "pants" => "XXL",
+                "shoes" => 43.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 24,
+                "shirt" => "S",
+                "pants" => "S",
+                "shoes" => 37.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 25,
+                "shirt" => "M",
+                "pants" => "L",
+                "shoes" => 40.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 26,
+                "shirt" => "L",
+                "pants" => "XL",
+                "shoes" => 41.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 27,
+                "shirt" => "XL",
+                "pants" => "XXL",
+                "shoes" => 43.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 28,
+                "shirt" => "S",
+                "pants" => "M",
+                "shoes" => 38.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 29,
+                "shirt" => "M",
+                "pants" => "L",
+                "shoes" => 40.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "user" => 30,
+                "shirt" => "L",
+                "pants" => "XL",
+                "shoes" => 41.0,
+                "created_at" => now(),
+                "updated_at" => now()
+            ]
+        ]);
     }
 }
