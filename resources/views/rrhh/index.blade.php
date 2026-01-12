ac6b3b8b29871d6eb0d354c1fb47ea72c79fac73
Pa-u-G <pau.garbajosa@inslapineda.cat>
2026-01-12 22:52:10 +0100

    tema oscuro del index y show de RRHH y mantenimientos

--- resources/views/rrhh/index.blade.php
+++ resources/views/rrhh/index.blade.php
@@ -2,7 +2,7 @@
 @section("title", "Veure els centres")
 @section("main")
 <div class="flex items-center justify-between mb-7">
-    <h1 class="text-3xl font-bold text-[#011020]">Gestió de temes pendents RRHH: </h1>
+    <h1 class="text-3xl font-bold text-[#011020] dark:text-white">Gestió de temes pendents RRHH: </h1>
 
     <a  href="{{ route("rrhh.create") }}" class="bg-[#FF7E13] text-white rounded-lg p-2 font-semibold flex items-center justify-center cursor-pointer gap-2 hover:bg-[#FE712B] transition-all">
         <svg class="w-6 h-6 text-white">
@@ -13,7 +13,7 @@
 </div>
 <div class="flex flex-row gap-2 md:gap-5">
     <!-- Barra de busqueda -->
-    <form action="{{ route("rrhh.search") }}" method="post" data-type="rrhh" class="searchForm w-[95%] flex items-center gap-2 border-1 border-[#E6E5DE] rounded-lg h-10 bg-white p-5">
+    <form action="{{ route("rrhh.search") }}" method="post" data-type="rrhh" class="searchForm w-[95%] flex items-center gap-2 border border-[#E6E5DE] rounded-lg h-10 bg-white p-5 dark:bg-neutral-800 dark:border-neutral-600 dark:text-white">
         @csrf
         <button type="submit" class="cursor-pointer">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#AFAFAF" class="size-6">
@@ -28,11 +28,11 @@
     </div>
     <!-- Filtros -->
     <div class="flex flex-row justify-between gap-2">
-        <button data-modal-id="filterContainer" class="open-modal-button bg-white text-[#011020] rounded-lg p-2 font-semibold flex items-center justify-center cursor-pointer gap-2 border-1 border-[#AFAFAF]">
+        <button data-modal-id="filterContainer" class="open-modal-button bg-white text-[#011020] rounded-lg p-2 font-semibold flex items-center justify-center cursor-pointer gap-2 border border-[#AFAFAF] dark:bg-neutral-800 dark:border-neutral-500 dark:text-white dark:hover:bg-neutral-600">
             <svg class="w-6 h-6">
                 <use xlink:href="#icon-adjustments-horizontal"></use>
             </svg>
-            Filtres
+            <p class="hidden md:block">Filtres</p>
         </button>
     </div>
         <button id="changeView" class="bg-white text-[#011020] rounded-lg p-2 font-semibold flex items-center justify-center cursor-pointer gap-2 border border-[#AFAFAF] dark:bg-neutral-800 dark:border-neutral-500 dark:text-white dark:hover:bg-neutral-600">
