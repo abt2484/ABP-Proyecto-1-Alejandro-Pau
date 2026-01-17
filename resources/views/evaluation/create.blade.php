3b90c3ece3af28a40069f82f0caeb9beace4ffa4
Pa-u-G <pau.garbajosa@inslapineda.cat>
2026-01-18 00:48:21 +0100

    tema oscuro terminado #133

--- resources/views/evaluation/create.blade.php
+++ resources/views/evaluation/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section("title", "Veure els seguiments")
 @section('main')
-<div class="min-w-fit w-9/10 mx-auto flex flex-col mb-7 gap-10">
+<div class="min-w-fit w-9/10 mx-auto flex flex-col mb-7 gap-10 dark:text-white">
     <!-- Header -->
     <div class="w-full flex flex-row justify-between items-center">
         <div class="w-fit flex flex-col gap-5">
@@ -11,28 +11,28 @@
                 </svg>
                 Tornar a la gestió de professionals
             </a>
-            <h1 class="text-3xl font-bold text-[#011020]">Avaluacions de {{ $user->name }}</h1>
+            <h1 class="text-3xl font-bold text-[#011020] dark:text-white">Avaluacions de {{ $user->name }}</h1>
             <p class="text-[#AFAFAF]" >Avaluacions del professional seleccionat</p>
         </div>
     </div>
     <form action="{{ route('evaluations.store', $user->id) }}" method="POST">
         @csrf
         @method("POST")
-        <div class="border border-[#AFAFAF] bg-white rounded-[15px] p-10 flex flex-col gap-5">
+        <div class="border border-[#AFAFAF] bg-white rounded-[15px] p-10 flex flex-col gap-5 dark:bg-neutral-800 dark:border-neutral-600">
             <div class="flex flex-col w-full">
                 <div class="flex flex-row sticky top-20 items-center h-20 pl-5 font-bold">
                     <div class="w-1/2 h-full"></div>
-                    <div class="w-1/8 border-1 border-[#AFAFAF] h-full flex items-center justify-center text-white bg-[#FF7E13]">Gens d’acord</div>
-                    <div class="w-1/8 border-1 border-[#AFAFAF] h-full flex items-center justify-center text-white bg-[#FF7E13]">Poc d’acord</div>
-                    <div class="w-1/8 border-1 border-[#AFAFAF] h-full flex items-center justify-center text-white bg-[#FF7E13]">Bastant d’acord</div>
-                    <div class="w-1/8 border-1 border-[#AFAFAF] h-full flex items-center justify-center text-white bg-[#FF7E13]">Molt d’acord</div>
+                    <div class="w-1/8 border-1 border-[#AFAFAF] h-full flex items-center justify-center text-white bg-[#FF7E13] dark:border-neutral-600">Gens d’acord</div>
+                    <div class="w-1/8 border-1 border-[#AFAFAF] h-full flex items-center justify-center text-white bg-[#FF7E13] dark:border-neutral-600">Poc d’acord</div>
+                    <div class="w-1/8 border-1 border-[#AFAFAF] h-full flex items-center justify-center text-white bg-[#FF7E13] dark:border-neutral-600">Bastant d’acord</div>
+                    <div class="w-1/8 border-1 border-[#AFAFAF] h-full flex items-center justify-center text-white bg-[#FF7E13] dark:border-neutral-600">Molt d’acord</div>
                 </div>
                 @for ($i = 1; $i <= 20; $i++)
-                <div class="flex flex-row items-center pl-5 h-20 border-b-1 border-b-[#AFAFAF]">
+                <div class="flex flex-row items-center pl-5 h-20 border-b-1 border-b-[#AFAFAF] dark:border-b-neutral-600">
                     <div class="w-1/2 font-bold text-lg">{{ $questions[$i-1] }}</div>
                     <div class="flex-1 flex justify-between">
                         @for ($j = 0; $j < 4; $j++)
-                        <div class="flex-1 border-1 border-[#AFAFAF] h-20 flex items-center justify-center" data-question="p{{ $i }}" data-value="{{ $j }}">
+                        <div class="flex-1 border-1 border-[#AFAFAF] dark:border-neutral-600 h-20 flex items-center justify-center" data-question="p{{ $i }}" data-value="{{ $j }}">
                             <input type="radio" name="p{{ $i }}" value="{{ $j }}" class="hidden">
                         </div>
                         @endfor
@@ -47,10 +47,10 @@
                     </svg>
                     Observacions
                 </div>
-                <textarea name="comment" id="comment" cols="30" rows="3" class="border border-[#AFAFAF] bg-white rounded-[15px] p-3" placeholder="Afegir una nova observació"></textarea>
+                <textarea name="comment" id="comment" cols="30" rows="3" class="border border-[#AFAFAF] bg-white rounded-[15px] p-3 dark:bg-neutral-800 dark:border-neutral-600" placeholder="Afegir una nova observació"></textarea>
             </div>
-            <div class="flex flex-row gap-4 justify-end mt-8 pt-6 border-t border-gray-200">
-                <a href="{{ route('evaluations.index', $user->id) }}" class="bg-white text-[#011020] rounded-lg p-2 font-semibold flex items-center justify-center cursor-pointer gap-2 border-1 border-[#AFAFAF]">
+            <div class="flex flex-row gap-4 justify-end mt-8 pt-6 border-t border-gray-200 dark:border-neutral-600">
+                <a href="{{ route('evaluations.index', $user->id) }}" class="bg-white text-[#011020] rounded-lg p-2 font-semibold flex items-center justify-center cursor-pointer gap-2 border-1 border-[#AFAFAF] dark:bg-neutral-800 dark:border-neutral-600 dark:text-white">
                     Cancel·lar
                 </a>
                 <button type="submit" class="bg-[#FF7E13] text-white rounded-lg p-2 font-semibold flex items-center justify-center cursor-pointer gap-2 hover:bg-[#FE712B] transition-all">
--- resources/views/rrhh/tracking.blade.php
+++ resources/views/rrhh/tracking.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section("title", "Veure els seguiments")
 @section('main')
-<div class="min-w-fit w-9/10 mx-auto flex flex-col mb-7 gap-10">
+<div class="min-w-fit w-9/10 mx-auto flex flex-col mb-7 gap-10 dark:text-white">
     <!-- Header -->
     <div class="w-full flex flex-row justify-between items-center">
         <div class="w-fit flex flex-col gap-5">
@@ -11,7 +11,7 @@
                 </svg>
                 Tornar al tema pendent
             </a>
-            <h1 class="text-3xl font-bold text-[#011020]">Seguiment de: {{ $rrhh->topic }}</h1>
+            <h1 class="text-3xl font-bold text-[#011020] dark:text-white">Seguiment de: {{ $rrhh->topic }}</h1>
             <p class="text-[#AFAFAF]" >Comentaris del tema pendent</p>
         </div>
     </div>
@@ -19,9 +19,9 @@
 
         <div class="flex flex-row justify-between gap-5">
             <!-- Historial -->
-            <div class="flex flex-col justify-start w-5/8 gap-5 max-h-[640px] {{ $total<1 ? 'h-[170px]' : '' }} {{ $total==1 ? 'h-[360px]' : '' }} border border-[#AFAFAF] bg-white rounded-[15px] p-5">
+            <div class="flex flex-col justify-start w-5/8 gap-5 max-h-[640px] {{ $total<1 ? 'h-[170px]' : '' }} {{ $total==1 ? 'h-[360px]' : '' }} border border-[#AFAFAF] bg-white rounded-[15px] p-5 dark:bg-neutral-800 dark:border-neutral-600">
                 <div class="flex flex-row justify-between items-center w-full">
-                    <div class="text-2xl font-bold text-[#011020]">
+                    <div class="text-2xl font-bold text-[#011020] dark:text-white">
                         Historial de comentaris
                     </div>
                     <div class="text-[#FF7E13] text-1xl font-bold">
@@ -32,19 +32,19 @@
                     @foreach ($comments as $comment )
                         <div class="flex flex-row gap-5">
                             <div class="flex flex-col">
-                                <div class="bg-zinc-100 w-14 h-14 rounded-full border-3 border-zinc-300">
+                                <div class="bg-zinc-100 w-14 h-14 rounded-full border-3 border-zinc-300 dark:border-neutral-500">
                                     <minidenticon-svg username="{{ md5($comment->userRelation->id) }}"></minidenticon-svg>
                                 </div>
                                 <div class="min-h-[125px] h-full flex justify-center">
-                                    <div class="bg-zinc-300 p-0.5 h-full"></div>
+                                    <div class="bg-zinc-300 p-0.5 h-full dark:bg-neutral-500"></div>
                                 </div>
                             </div>
-                            <div class="border border-[#AFAFAF] bg-white rounded-[15px] p-5 w-full h-fit flex flex-col gap-5 overflow-hidden break-words whitespace-normal break-all mb-5">
+                            <div class="border border-[#AFAFAF] bg-white rounded-[15px] p-5 w-full h-fit flex flex-col gap-5 overflow-hidden break-words whitespace-normal break-all mb-5 dark:bg-neutral-800 dark:border-neutral-600">
                                 <div class="flex flex-row justify-between min-w-max">
                                     <div class="text-xl font-bold">
                                         {{ $comment->userRelation->name }}
                                     </div>
-                                    <div class="flex flex-row gap-3 text-[#5E6468]">
+                                    <div class="flex flex-row gap-3 text-[#5E6468] dark:text-white">
                                         <svg class="w-6 h-6">
                                             <use xlink:href="#icon-calendar"></use>
                                         </svg>
@@ -64,12 +64,12 @@
                         <div class='mb-4'>No hi ha commentaris</div>
                     @endif -->
                     <div class="flex flex-row gap-5 items-center">
-                        <div class="bg-zinc-200 p-[19px] w-fit rounded-full border-3 border-zinc-300">
-                            <div class="bg-zinc-300 w-[11px] h-[11px] rounded-full">
+                        <div class="bg-zinc-200 p-[19px] w-fit rounded-full border-3 border-zinc-300 dark:border-neutral-500 dark:bg-neutral-400">
+                            <div class="bg-zinc-300 w-[11px] h-[11px] rounded-full dark:bg-neutral-500">
     
                             </div>
                         </div>
-                        <div class="text-zinc-500">
+                        <div class="text-zinc-300 dark:border-neutral-500">
                             Fi de l'historial
                         </div>
                     </div>
@@ -77,7 +77,7 @@
             </div>
             @unless ($rrhh->end_link)
                 <!-- Formulario -->
-                <div class="flex flex-col justify-center w-1/4 h-fit border border-[#AFAFAF] bg-white rounded-[15px] p-5 gap-5 max-h-[640px] min-w-min">
+                <div class="flex flex-col justify-center w-1/4 h-fit border border-[#AFAFAF] bg-white rounded-[15px] p-5 gap-5 max-h-[640px] min-w-min dark:bg-neutral-800 dark:border-neutral-600">
                     <div class="pb-3 border-b-1 border-[#AFAFAF] flex flex-col gap-2">
                         <div class="flex flex-row gap-3">
                             <svg class="w-6 h-6 text-[#FF7E13]">
@@ -87,17 +87,17 @@
                                 Nou comentari 
                             </div>
                         </div>
-                        <div class="text-[#5E6468]">
+                        <div class="text-[#5E6468] dark:text-white">
                             Afegeix un comentari nou dins del tema pendent: {{ $rrhh->topic }}
                         </div>
                     </div>
-                    <form action="{{ route('rrhh.tracking.store', $rrhh) }}" method="POST" class="text-[#5E6468] pt-5 flex flex-col gap-7">
+                    <form action="{{ route('rrhh.tracking.store', $rrhh) }}" method="POST" class="text-[#5E6468] dark:text-white pt-5 flex flex-col gap-7">
                         @csrf
                         @method("POST")
                         <div class="flex flex-col gap-3">
                             <div class="flex flex-col gap-1">
                                 <label for="comment">Afegir comentari</label>
-                                <textarea type="text" id="comment" name="comment" class="border border-[#AFAFAF] bg-white rounded-lg p-2 max-h-[360px] h-[200px]"></textarea>
+                                <textarea type="text" id="comment" name="comment" class="border border-[#AFAFAF] bg-white rounded-lg p-2 max-h-[360px] h-[200px] dark:bg-neutral-800 dark:border-neutral-600"></textarea>
                             </div>
                         </div>
                         <button type="submit" class="bg-[#FF7E13] text-white rounded-lg p-2 font-semibold flex items-center justify-center cursor-pointer gap-2 hover:bg-[#FE712B] transition-all">
