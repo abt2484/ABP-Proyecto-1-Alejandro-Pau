0d32a7037dcc52876db7f9ddad4343bc8730c338
Pa-u-G <pau.garbajosa@inslapineda.cat>
2026-01-22 10:52:40 +0100

    accidentabilidad

--- database/migrations/2025_09_29_072320_create_accidentalities_table.php
+++ database/migrations/2025_09_29_072320_create_accidentalities_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('accidentabilites', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger("low");
+            $table->unsignedBigInteger("sick");
             $table->timestamp("start")->nullable();
             $table->timestamp("end")->nullable();
             $table->string("context", 255);
@@ -23,7 +23,7 @@ return new class extends Migration
 
             $table->timestamps();
 
-            $table->foreign("low")->references("id")->on("users");
+            $table->foreign("sick")->references("id")->on("users");
             $table->foreign("evaluate")->references("id")->on("users");
         });
     }
